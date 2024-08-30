06f5959c0699596fdb4d1f5b40ae78c5289d10ea
User <esipov@sv-en.ru>
2024-08-31 02:54:09 +0300

    Добавлен Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {languageOptions: { globals: globals.browser },
-  rules: {
-    "no-console" : "error", 
-    // "plugins": ["jest"]
-  }
-},
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-console": "error",
+      // ignores: ["**/*.test.js", "!**/eslint.config.js"],
+      // "plugins": ["jest"]
+    },
+  },
+  eslintConfigPrettier,
   pluginJs.configs.recommended,
 ];
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,6 +1,6 @@
 import { sum } from "./sum";
-describe("Sum",()=>{
-    it("return sum of two numbers", ()=>{
-        expect(sum(1,2)).toEqual(3);
-    })
+describe("Sum", () => {
+  it("return sum of two numbers", () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
 });
