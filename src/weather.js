44f347aaf9b49dab83c007847f12fece5e5e1a88
User <esipov@sv-en.ru>
2024-09-14 22:13:57 +0300

    Изменяем апи погоды_2

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,7 @@ async function getWeather(city) {
    
     /*eslint-disable-next-line no-undef*/
     let data = await openweathermapApi(city);
-    let tempCity = data.main.temp;
+    let tempCity = data.list[0].main.temp;
     /*eslint-enable-next-line*/
 
 return tempCity - 273.15;
@@ -17,8 +17,7 @@ const buttonElement = document.querySelector('#searchButton');
 buttonElement.addEventListener('click', function() {
     let city = document.querySelector('#search').value;
     // eslint-disable-next-line no-unused-vars
-    let translateCity = translateCityName(city, apiKey);
-    print(translateCity);
+    print(city);
 });
 
 
@@ -26,10 +25,15 @@ async function print(city){
     // eslint-disable-next-line no-console
     console.log(await getWeather(city));
     
+        const element = document.querySelector('#weather');
+
+          element.innerText = await getWeather(city);
+
 }
+    
 
 
-// eslint-disable-next-line no-unused-vars
-let apiKey = '53053fd8-cdad-4bd7-ac64-5729c3a42021';
+
+
 
 
--- src/workAPI.js
+++ src/workAPI.js
@@ -1,4 +1,4 @@
-const URL = "https://api.openweathermap.org/data/2.5/weather?";
+const URL = "https://openweathermap.org/data/2.5/find?";
 let keyAPI = "439d4b804bc8187953eb36d2a8c26a02&_=1726339180386";
 
 
@@ -8,29 +8,9 @@ let keyAPI = "439d4b804bc8187953eb36d2a8c26a02&_=1726339180386";
  * @param {string} q - Город
  */
 
-async function requestGeocodingAPI(q) {
-  let response = await fetch(
-      URL + 'q=' + q + '&limit=1&appid=' + keyAPI,
-      {
-        method: "GET",
-      },
-    );
-
-    if (!response.ok) {
-      throw new Error(`Ошибка HTTP: ${response.status}`);
-    }
-
-    let responseJson = await response.json();
-    return responseJson.coord;
-}
-
-
-
 async function openweathermapApi(city) {
-  let coordinates = await requestGeocodingAPI(city);
-  
-  let response = await fetch(
-      URL + 'lat=' + coordinates.lat + '&lon=' + coordinates.lon + '&appid=' + keyAPI,
+    let response = await fetch(
+      URL + 'q=' + city + '&limit=1&appid=' + '&appid=' + keyAPI,
       {
         method: "GET",
       },
@@ -44,10 +24,6 @@ async function openweathermapApi(city) {
     return responseJson;
 }
 
-/*
-eslint-disable-next-line no-undef
-*/
+/*eslint-disable-next-line no-undef*/
 window.openweathermapApi = openweathermapApi;
-/*
-eslint-enable-next-line
-*/
+/*eslint-enable-next-line*/
