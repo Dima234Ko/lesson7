f52463b2d544038f471c629c2891bc9c1c462210
User <esipov@sv-en.ru>
2024-09-15 11:20:44 +0300

    Работа с апи запросом

--- src/weather.js
+++ src/weather.js
@@ -1,33 +1,44 @@
 async function getWeather(city) {
-   
-    /*eslint-disable-next-line no-undef*/
     let data = await openweathermapApi(city);
-    let tempCity = data.list[0].main.temp;
-    /*eslint-enable-next-line*/
-
-return tempCity - 273.15;
-    
+    return data ;   
 }
 
-
-// eslint-disable-next-line no-unused-vars
 const buttonElement = document.querySelector('#searchButton');
 
 // Добавляем обработчик события на кнопку
 buttonElement.addEventListener('click', function() {
     let city = document.querySelector('#search').value;
-    // eslint-disable-next-line no-unused-vars
     print(city);
 });
 
 
 async function print(city){
-    // eslint-disable-next-line no-console
-    console.log(await getWeather(city));
+    let data = await getWeather(city);
+    let tempCity = (data.list[0].main.temp - 273.15).toFixed(0);
+    let windSpeed = data.list[0].wind.speed;
+    let humidity = data.list[0].main.humidity;
+    let pressure = (data.list[0].main.pressure * 0.987).toFixed(0);
+    let condition;
+    
+    switch (data.list[0].weather[0].main) {
+        case 'Clouds':
+            // eslint-disable-next-line no-case-declarations, no-unused-vars
+            condition = 'Облачно';
+            break;
+        default:
+          // eslint-disable-next-line no-console
+          console.log('Sorry');
+      }
+
     
         const element = document.querySelector('#weather');
 
-          element.innerText = await getWeather(city);
+          element.innerText = 'Температура: ' + tempCity + '°C' + '\n' +
+          'Ветер: ' + windSpeed + ' м/с' + '\n' +
+          'Влажность: ' + humidity + '%' + '\n' +
+          'Давление: ' + pressure + ' атм' + '\n' +  
+          // eslint-disable-next-line no-undef
+          condition;
 
 }
     
