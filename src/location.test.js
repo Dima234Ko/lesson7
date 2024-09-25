5347a58f7207be1024078bb0377c0ea5320c5939
User <esipov@sv-en.ru>
2024-09-25 15:22:16 +0300

    добавлен README.md, покрытие тестами 63%

--- src/location.test.js
+++ src/location.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { main } from "./location";
+import { main, getUserCoordinates } from "./location";
 
 describe("main function", () => {
   it("Проверка, что возвращается объект с координатами", async () => {
@@ -7,5 +7,43 @@ describe("main function", () => {
     expect(result).toBeInstanceOf(Object);
     expect(result).toHaveProperty("lat");
     expect(result).toHaveProperty("lon");
+  });
+});
+
+describe("getUserCoordinates", () => {
+  beforeEach(() => {
+    // Mock the navigator.geolocation object
+    global.navigator.geolocation = {
+      getCurrentPosition: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    // Reset the mock
+    jest.resetAllMocks();
+  });
+
+  it("Возврат координат", async () => {
+    global.navigator.geolocation.getCurrentPosition.mockImplementation(
+      (success) => {
+        success({
+          coords: {
+            latitude: 37.7749,
+            longitude: -122.4194,
+          },
+        });
+      },
+    );
+
+    const coordinates = await getUserCoordinates();
+    expect(coordinates).toEqual([37.7749, -122.4194]);
+  });
+
+  it("сообщение об ошибке, если геолокация не поддерживается", async () => {
+    global.navigator.geolocation = null;
+
+    await expect(getUserCoordinates()).rejects.toThrowError(
+      "Geolocation is not supported by this browser",
+    );
   });
 });
